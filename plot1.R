7d6f1b6be12466275af856c778695f2c9d729882
kmartie <katyamartynkina7@gmail.com>
2020-11-07 21:44:06 +0200

    constructs the plot1.png

---
+++ plot1.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+my_data <- read.delim("household_power_consumption.txt",
+                      sep = ";",
+                      colClasses = "character",
+                      nrows = 2075259,
+                      stringsAsFactors = FALSE)
+
+my_data <- my_data %>%  filter(Date == "1/2/2007" | Date == "2/2/2007") %>% 
+  mutate_at(vars(Global_active_power), as.numeric)
+
+
+png(file = "plot1.png")
+with(my_data, hist(Global_active_power,
+                   main = "Global Active Power",
+                   col = "red",
+                   xlab = "Global Active Power (kilowatts)"))
+dev.off()
