a53e5aded76918974d2caa4b885282edd9aad821
kmartie <katyamartynkina7@gmail.com>
2020-11-08 20:17:41 +0200

    constructing plot 2

--- plot2.R
+++ plot2.R
@@ -11,7 +11,7 @@ data <- read.delim("household_power_consumption.txt",
 
 my_data <- data %>%  filter(Date == "1/2/2007" | Date == "2/2/2007") %>% 
   mutate_at(vars(Global_active_power), as.numeric) %>% 
-  mutate_at(vars(Date), ~dmy_hms(paste(Date, Time), tz = "America/New_York"))
+  mutate_at(vars(Date), ~dmy_hms(paste(Date, Time)))
  
 png(file = "plot2.png") 
 with(my_data, plot(Date, Global_active_power,
